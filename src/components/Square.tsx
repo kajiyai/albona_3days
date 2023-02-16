94a9fbd8c5129c63413117d7e815798d35add4dd
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-02-16 15:26:01 +0900

    Components are now separated into one component per file.

---
+++ src/components/Square.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../index.css";
+
+interface SquareProps {
+  value: string | null;
+  onClick: () => void;
+}
+
+export function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
