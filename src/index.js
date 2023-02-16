86596eaa37cfcc4eabeaf21e87c5c719ecfae323
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-02-16 11:26:04 +0900

    Click to view X

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: "x" })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
